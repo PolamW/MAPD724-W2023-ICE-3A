6a0c0312b30ca41d6f435a7ce1494db5f62ee5b8
PolamW <pwong140@my.centennialcollege.ca>
2023-02-05 19:50:36 -0500

    added project files for ICE 3

--- MAPD724-W2023-ICE-3A/GameViewController.swift
+++ MAPD724-W2023-ICE-3A/GameViewController.swift
@@ -1,9 +1,3 @@
-//
-//  GameViewController.swift
-//  MAPD724-W2023-ICE-3A
-//
-//  Created by Po Lam Wong on 5/2/2023.
-//
 
 import UIKit
 import SpriteKit
@@ -11,42 +5,24 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
+        if let scene = GKScene(fileNamed: "GameScene")
+        {
             if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
+                if let view = self.view as! SKView?
+                {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
                 }
             }
         }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+            return .portrait
     }
 
     override var prefersStatusBarHidden: Bool {
